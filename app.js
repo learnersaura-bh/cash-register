734d9e299c346c6968a7fa8ae6145f0801b77b27
learnersaura-bh <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-15 17:01:46 +0530

    docs-final-update

--- app.js
+++ app.js
@@ -2,34 +2,33 @@ var billAmount = document.querySelector("#bill-amount");
 var cashAmount = document.querySelector("#cash-amount");
 var btnCheck = document.querySelector("#btn-check");
 var errorMessage = document.querySelector("#error-message");
-var noOfNotes = document.querySelector("no-of-notes")
-var notesWeHave = [ 2000, 500, 100, 50, 10, 2, 1];
+var noOfNotes = document.querySelectorAll(".no-of-notes");
+var notesWeHave = [ 2000, 500, 100, 20, 10 ,5 , 1];
 
-function clickEventHandler(){
+
+btnCheck.addEventListener("click" , function clickEventHandler(){
     if(billAmount.value>0){
         if(cashAmount.value > billAmount.value){
- var cashToBeReturned = cashAmount.value - billAmount.value;
+ const cashToBeReturned = cashAmount.value - billAmount.value;
  calculateChange(cashToBeReturned);
         }
-        else{errorMessage.innerText = "Customer needs to pay more than or equal to bill amount"}
-
-  } 
+        else{errorMessage.innerText = "Customer needs to pay more than or equal to bill amount"}} 
   else{
-    // errorMessage.innerText = "Input amount is not valid, amount should be greater than zero" }
-}};
-
-
+    errorMessage.innerText = "Input amount is not valid, amount should be greater than zero" }
+} );
 
 function calculateChange(cashToBeReturned){
-    for(var i=0; i<notesWeHave.length;i++){
-        var numberOfNotes = Math.trunc(cashToBeReturned/notesWeHave[i]);
-        cashToBeReturned %= notesWeHave[i];
+    for(let i=0; i < notesWeHave.length; i++){
+        const numberOfNotes = Math.trunc(
+            cashToBeReturned / notesWeHave[i]
+            );
+        cashToBeReturned= cashToBeReturned % notesWeHave[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
 
-
+   
 }
 
 
-btnCheck.addEventListener("click" , clickEventHandler )
+
 
