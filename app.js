ba2884441d2b0ac7c319a36ff66c19a4449c8e17
learnersaura-bh <119805882+learnersaura-bh@users.noreply.github.com>
2022-12-21 09:43:19 +0530

    fix-errors

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 var billAmount = document.querySelector("#bill-amount");
 var cashAmount = document.querySelector("#cash-amount");
 var btnCheck = document.querySelector("#btn-check");
-var errorMessage = document.querySelector("#error-message");
+var showMessage = document.querySelector("#error-message");
 var noOfNotes = document.querySelectorAll(".no-of-notes");
 var notesWeHave = [ 2000, 500, 100, 20, 10 ,5 , 1];
 
 
 btnCheck.addEventListener("click" , function clickEventHandler(){
     if(billAmount.value>0){
-        if(cashAmount.value >= billAmount.value){
- const cashToBeReturned = cashAmount.value - billAmount.value;
+        if(Number(cashAmount.value) > Number(billAmount.value)){
+ const cashToBeReturned = Number(cashAmount.value) - Number(billAmount.value);
  calculateChange(cashToBeReturned);
         }
-        else{errorMessage.innerText = "Customer needs to pay more than or equal to bill amount"}} 
+        else if(Number(cashAmount.value) === Number(billAmount.value)){
+            showMessage.innerText = "Bill is paid, no need to return cash"
+        }
+        else{showMessage.innerText = "Customer needs to pay more than or equal to bill amount"}} 
   else{
-    errorMessage.innerText = "Input amount is not valid, amount should be greater than zero" }
+    showMessage.innerText = "Input amount is not valid, amount should be greater than zero" }
 } );
 
 function calculateChange(cashToBeReturned){
